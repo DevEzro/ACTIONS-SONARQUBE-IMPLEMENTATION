3df26d892933e62af29edebed3d2b83d71676550
DevEzro <davidtestout@outlook.com>
2025-04-07 14:48:50 +0200

    changed file.py

--- python/file.py
+++ python/file.py
@@ -2,8 +2,4 @@ def test_sum():
     a, b = 12, 31
     result = a + b
     print(f"{a} + {b} = {result}")
-<<<<<<< HEAD
-    assert result == 43
-=======
     assert result == 43 # nosec
->>>>>>> feature
