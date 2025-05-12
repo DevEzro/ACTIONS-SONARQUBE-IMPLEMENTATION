bb2056c191feaf49ae9eebdefe78286a7dae09ea
DevEzro <davidtestout@outlook.com>
2025-05-12 14:41:59 +0200

    init repo

--- src/com/example/sqlinjectiontest/Main.java
+++ src/com/example/sqlinjectiontest/Main.java
@@ -3,7 +3,7 @@ package com.example.sqlinjectiontest;
 public class Main{
 
     public static void main(String[] args){
-        System.out.println("Hello world");
+        System.out.println("Hello world!");
     }
 
 }
