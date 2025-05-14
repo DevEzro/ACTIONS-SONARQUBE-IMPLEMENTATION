0b1c35ca4bc52c5c895d22d7a1a21723a1aef033
DevEzro <davidtestout@outlook.com>
2025-05-14 08:58:20 +0200

    add SQL INJECTION vuln

--- src/com/example/sqlinjectiontest/Main.java
+++ src/com/example/sqlinjectiontest/Main.java
@@ -1,9 +1,24 @@
-package com.example.sqlinjectiontest;
 
-public class Main{
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.Scanner;
 
-    public static void main(String[] args){
-        System.out.println("Hello world");
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); //Init data input
+
+        System.out.println("Type your ID: ");
+        String userId = scanner.nextLine();
+
+        String query = "SELECT * FROM users WHERE id = " + userId; //Query
+
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "password"); //Connection to database
+            Statement stmt = conn.createStatement();
+            stmt.execute(query);
+        } catch (Exception e) {
+            e.printStackTrace(); //Catch any error
+        }
     }
-    
 }
