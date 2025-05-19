65154478d264b94cf32853e41c2b78e894723325
DevEzro <davidtestout@outlook.com>
2025-05-19 11:13:46 +0200

    test

--- src/com/example/sqlinjectiontest/Main.java
+++ src/com/example/sqlinjectiontest/Main.java
@@ -1,11 +1,8 @@
 package com.example.sqlinjectiontest;
-import java.util.*;
 
 public class Main {
-    private static final Logger logger = Logger.getLogger(MiClase.class.getName());
-
     public static void main(String[] args) {
-        logger.info("Resultado suma: " + sumar(2, 3));
+        System.out.println("Resultado suma: " + sumar(2, 3));
     }
 
     public static int sumar(int a, int b) {
