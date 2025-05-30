244bfeafd7299941e4a036a056d1d4fce8ffeecb
DevEzro <92102267+DevEzro@users.noreply.github.com>
2025-05-30 08:16:46 +0200

    Delete python directory

--- python/file.py
+++
@@ -1,5 +0,0 @@
-def test_sum():
-    a, b = 12, 31
-    result = a + b
-    print(f"{a} + {b} = {result}")
-    assert result == 43 # nosec -> especifica a bandit que no es para un test de seguridad
